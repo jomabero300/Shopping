efc3527c55a2af84b19cd2504ffb148c722ac21c
jomabero300 <jomabero300@yahoo.com>
2022-07-12 15:04:36 -0500

    Add Details Home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,32 +29,8 @@ public class HomeController : Controller
             .Include(p => p.ProductCategories)
             .OrderBy(p => p.Description)
             .ToListAsync();
-        List<ProductsHomeViewModel> productsHome = new() { new ProductsHomeViewModel() };
-        int i = 1;
-        foreach (Product product in products)
-        {
-            if (i == 1)
-            {
-                productsHome.LastOrDefault().Product1 = product;
-            }
-            if (i == 2)
-            {
-                productsHome.LastOrDefault().Product2 = product;
-            }
-            if (i == 3)
-            {
-                productsHome.LastOrDefault().Product3 = product;
-            }
-            if (i == 4)
-            {
-                productsHome.LastOrDefault().Product4 = product;
-                productsHome.Add(new ProductsHomeViewModel());
-                i = 0;
-            }
-            i++;
-        }
 
-        HomeViewModel model = new() { Products = productsHome };
+        HomeViewModel model = new() { Products = products };
         User user = await _userHelper.GetUserAsync(User.Identity.Name);
         if (user != null)
         {
@@ -64,7 +40,79 @@ public class HomeController : Controller
         }
 
         return View(model);
+    }
 
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        Product product = await _context.Products
+            .Include(p => p.ProductImages)
+            .Include(p => p.ProductCategories)
+            .ThenInclude(pc => pc.Category)
+            .FirstOrDefaultAsync(p => p.Id == id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        string categories = string.Empty;
+        foreach (ProductCategory category in product.ProductCategories)
+        {
+            categories += $"{category.Category.Name}, ";
+        }
+        categories = categories.Substring(0, categories.Length - 2);
+
+        AddProductToCartViewModel model = new()
+        {
+            Categories = categories,
+            Description = product.Description,
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            ProductImages = product.ProductImages,
+            Quantity = 1,
+            Stock = product.Stock,
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Details(AddProductToCartViewModel model)
+    {
+        if (!User.Identity.IsAuthenticated)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        Product product = await _context.Products.FindAsync(model.Id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        User user = await _userHelper.GetUserAsync(User.Identity.Name);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        TemporalSale temporalSale = new()
+        {
+            Product = product,
+            Quantity = model.Quantity,
+            Remarks = model.Remarks,
+            User = user
+        };
+
+        _context.TemporalSales.Add(temporalSale);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
     }
 
     public IActionResult Privacy()
